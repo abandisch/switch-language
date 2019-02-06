0ca06345a3a6119d39f6c45609fbbb06fe8920aa
Alex Bandisch <alexander.bandisch@gmail.com>
2019-02-07 09:20:55 +1000

    add button to switch language

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import Language from './Language'
 import { connect } from 'react-redux'
 import logo from './logo.svg'
 import languages from './languages'
+import { setLanguage } from './redux/actions'
 import './App.css'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.onSwitchLanguage = this.onSwitchLanguage.bind(this)
+  }
+
+  onSwitchLanguage (langCode) {
+    const { onSetLanguage } = this.props
+    onSetLanguage(langCode)
+  }
+
   render () {
     const { language: langCode } = this.props
     return (
@@ -18,6 +30,17 @@ class App extends Component {
             <strong>goodbye</strong>: {languages[langCode].goodbye}<br />
             <strong>hotel</strong>: {languages[langCode].hotel}<br />
           </p>
+          <div>
+            <button
+              style={{ padding: 10, fontSize: 16, marginRight: 10 }}
+              onClick={() => this.onSwitchLanguage('it')}
+            >Switch to Italian</button>
+
+            <button
+              style={{ padding: 10, fontSize: 16 }}
+              onClick={() => this.onSwitchLanguage('en')}
+            >Switch to English</button>
+          </div>
         </header>
       </div>
     )
@@ -25,5 +48,6 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({ someProp: 'fake prop' })
+const mapDispatchToProps = dispatch => ({ onSetLanguage: (langCode) => dispatch(setLanguage(langCode)) })
 
-export default Language()(connect(mapStateToProps)(App))
+export default Language()(connect(mapStateToProps, mapDispatchToProps)(App))
